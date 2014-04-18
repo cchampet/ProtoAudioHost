eae8be45017400e84efc801c14624b45e58bfc70
Clement Champetier <cnt@mikrosimage.eu>
2014-04-18 17:11:29 +0200

    ProtoLv2Host - simplify input / output / buffer memory management.

--- CustomLv2Host/main.cpp
+++ CustomLv2Host/main.cpp
@@ -2,23 +2,44 @@
 #include <cstring> //memcpy
 #include <string>
 #include <math.h> 
+#include <vector>
 
 // Lilv wrapper C++
 #include <lilv/lilvmm.hpp>
 // Tools to print Lv2 data
 #include "debugger.h"
 
+#include <iostream>
 
 /**
 * Proto which read test.wav, process a gain of 5db, and write testVFX.wav
 */
 int main(int argc, char** argv)
 {
+  // audio data - input / output / buffers
+  SndfileHandle infile( "test.wav" );
+  
+  const int format = infile.format();
+  const int numChannels = infile.channels();
+  const int samplerate = infile.samplerate();
+  const int timeToRead = 10; //in seconds
+
+  std::vector< short > audioIn( samplerate * numChannels * timeToRead, 0 );
+  std::vector< short > audioOut( samplerate * numChannels * timeToRead, 0 );
+  
+  SndfileHandle outfile( "testVFX.wav" , SFM_WRITE, format , numChannels , samplerate);
+
+  std::vector< std::vector< short > > audioBuffers;
+  std::vector< short > controlInputBuffer;
+  std::vector< short > controlOutputBuffer;
+  unsigned int audioBufferSize = 1024;
+  unsigned int idInputAudioPort = 0;
+  unsigned int idOutputAudioPort = 0;
+
   // load data of all lv2 plugins on the system
   Lilv::World* world = new Lilv::World();
   world->load_all();
   Lilv::Plugins plugins = ( Lilv::Plugins )world->get_all_plugins();
-  //lv2::tools::Debugger::list_plugins(plugins);
 
   // get plugin for the amplifier (gain)
   std::string pluginURI = "http://lv2plug.in/plugins/eg-amp";
@@ -26,43 +47,21 @@ int main(int argc, char** argv)
   Lilv::Plugin plugin = plugins.get_by_uri( plugin_uri );
   lv2::tools::Debugger::print_plugin( world->me, plugin.me );
 
-  // audio data
-  int samplerate = 8000;
-  int* audioIn = new int[ samplerate * 10 ];
-  int* audioOut = new int[ samplerate * 10 ];
-  // char audioIn [ samplerate * format * channels * 10 ];
-  // char audioOut[ samplerate * format * channels * 10 ];
-  //std::vector<char> audioIn( samplerate * format * channels * 10, 0 );
-
-  float** audioBuffers;
-  float* controlInputBuffer;
-  float* controlOutputBuffer;
-  unsigned int audioBufferSize = 1024;
-  unsigned int idInputAudioPort = 0;
-  unsigned int idOutputAudioPort = 0;
-  unsigned int idInputControlPort = 0;
-  unsigned int idOutputControlPort = 0;
-
   // create a node
   Lilv::Instance* instance = Lilv::Instance::create( plugin, samplerate, NULL );
   instance->activate();
   
   // connect ports
   unsigned int numPorts = plugin.get_num_ports();
-  audioBuffers = new float*[numPorts];
-  controlInputBuffer = new float[numPorts];
-  controlOutputBuffer = new float[numPorts];
-  for ( unsigned int i = 0; i < numPorts; i++ )
-  {
-    controlInputBuffer[i] = 0;
-    controlOutputBuffer[i] = 0;
-  }
+  audioBuffers = std::vector< std::vector< short > >( numPorts );
+  controlInputBuffer = std::vector< short >( numPorts, 0 );
+  controlOutputBuffer = std::vector< short >( numPorts, 0 );
 
   // create some nodes that represent certain features: Input / Output, Audio, Control (other exist : MIDI, Event...)
   Lilv::Node audio    = world->new_uri( LILV_URI_AUDIO_PORT );
-  Lilv::Node control    = world->new_uri( LILV_URI_CONTROL_PORT );
+  Lilv::Node control  = world->new_uri( LILV_URI_CONTROL_PORT );
   Lilv::Node input    = world->new_uri( LILV_URI_INPUT_PORT );
-  Lilv::Node output    = world->new_uri( LILV_URI_OUTPUT_PORT );
+  Lilv::Node output   = world->new_uri( LILV_URI_OUTPUT_PORT );
 
   for (unsigned int i = 0; i < numPorts; i++)
   {
@@ -70,21 +69,20 @@ int main(int argc, char** argv)
 
     if( port.is_a( audio ) && port.is_a( input ) )
     {
-      audioBuffers[i] = new float[audioBufferSize];
-      instance->connect_port( i, audioBuffers[i]);
+      audioBuffers[i] = std::vector< short >(audioBufferSize, 0);
+      instance->connect_port( i, &audioBuffers[i][0]);
       idInputAudioPort = i;
     }
     else if( port.is_a( audio ) && port.is_a( output ) )
     {
-      audioBuffers[i] = new float[audioBufferSize];
-      instance->connect_port( i, audioBuffers[i]);
+      audioBuffers[i] = std::vector< short >(audioBufferSize, 0);
+      instance->connect_port( i, &audioBuffers[i][0]);
       idOutputAudioPort = i;
     }
     
     else if( port.is_a( control ) && port.is_a( input ) )
     {
       instance->connect_port( i, &controlInputBuffer[i] );
-      idInputControlPort = i;
       // set param of the gain
       unsigned int gain = 5;
       controlInputBuffer[i] = gain;
@@ -92,7 +90,6 @@ int main(int argc, char** argv)
     else if( port.is_a( control ) && port.is_a( output ) )
     {
       instance->connect_port( i, &controlOutputBuffer[i] );
-      idOutputControlPort = i;
     }
     else
     {
@@ -101,18 +98,14 @@ int main(int argc, char** argv)
     }
   }
 
-  // define input and output file
-  const int format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-  unsigned int numChannels = 1;
-  SndfileHandle outfile( "testVFX.wav" , SFM_WRITE, format , numChannels , samplerate);
-  SndfileHandle infile( "test.wav" );
-  infile.read( audioIn, samplerate * 10 ); // read 10s of the input file
+  // read audio on disk
+  infile.read( &audioIn[0], samplerate * timeToRead );
 
   // process node
   int nframes = 128;
-  for (int i = 0; i < ceil( samplerate * 10 / nframes ); i++) {
+  for (int i = 0; i < ceil( samplerate * timeToRead / nframes ); i++) {
     // copy input
-    std::copy(&audioIn[ i * nframes ], &audioIn[ i * nframes ] + nframes, audioBuffers[idInputAudioPort]);
+    std::copy(&audioIn[ i * nframes ], &audioIn[ i * nframes ] + nframes, &audioBuffers[idInputAudioPort][0]);
     instance->run(nframes);
     // copy output
     for (int x = 0; x < nframes; x++) {
@@ -122,18 +115,7 @@ int main(int argc, char** argv)
   }
 
   // save audio to disk
-  outfile.write( audioOut, samplerate * 10 );
-  
-  // cleanup
-  delete[] controlInputBuffer;
-  delete[] controlOutputBuffer;
-  delete[] audioIn;
-  delete[] audioOut;
-  // for(int i = 0 ; i < numPorts; ++i)
-  // {
-  //   delete[] audioBuffers[i];
-  // }
-  // delete[] audioBuffers;
+  outfile.write( &audioOut[0], samplerate * timeToRead );
 
   return 0;
 }
