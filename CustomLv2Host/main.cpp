400a7196dc3193600bca926aa7fcdec83c9fd2a0
Clement Champetier <cnt@mikrosimage.eu>
2014-04-22 15:27:55 +0200

    CustomLv2Host - update audioBuffers array.

--- CustomLv2Host/main.cpp
+++ CustomLv2Host/main.cpp
@@ -10,10 +10,13 @@
 // Tools to print Lv2 data
 #include "debugger.h"
 
-// #define AUDIO_BUFFER_AUDIO_INPUT    0
-// #define AUDIO_BUFFER_AUDIO_OUTPUT   1
-// #define AUDIO_BUFFER_CONTROL_INPUT  2
-// #define AUDIO_BUFFER_CONTROL_OUTPUT 3
+#define AUDIO_BUFFER_AUDIO_INPUT    0
+#define AUDIO_BUFFER_AUDIO_OUTPUT   1
+#define AUDIO_BUFFER_CONTROL_INPUT  2
+#define AUDIO_BUFFER_CONTROL_OUTPUT 3
+// #define AUDIO_BUFFER_EVENT_INPUT    4
+// #define AUDIO_BUFFER_EVENT_OUTPUT   5
+// MIDI ?
 
 class Lv2Graph
 {
@@ -22,6 +25,8 @@ public:
   {
     pWorld = new Lilv::World();
     pWorld->load_all();
+
+    initAudioBuffers();
   }
 
   ~Lv2Graph()
@@ -57,42 +62,35 @@ public:
     } 
     int indexPlugin = std::distance(nodes.begin(), it);
 
-    unsigned int numPorts = plugins.at(indexPlugin).get_num_ports();
-    audioBuffers = std::vector< std::vector< short > >( numPorts );
-    controlInputBuffer = std::vector< short >( numPorts, 0 );
-    controlOutputBuffer = std::vector< short >( numPorts, 0 );
-
     // create some nodes that represent certain features: Input / Output, Audio, Control (other exist : MIDI, Event...)
     Lilv::Node audio    = pWorld->new_uri( LILV_URI_AUDIO_PORT );
     Lilv::Node control  = pWorld->new_uri( LILV_URI_CONTROL_PORT );
     Lilv::Node input    = pWorld->new_uri( LILV_URI_INPUT_PORT );
     Lilv::Node output   = pWorld->new_uri( LILV_URI_OUTPUT_PORT );
 
+    unsigned int numPorts = plugins.at(indexPlugin).get_num_ports();
     for (unsigned int i = 0; i < numPorts; i++)
     {
       Lilv::Port port = plugins.at(indexPlugin).get_port_by_index(i);
 
       if( port.is_a( audio ) && port.is_a( input ) )
       {
-        audioBuffers[i] = std::vector< short >(audioBufferSize, 0);
-        node1->connect_port( i, &audioBuffers[i][0]);
-        idInputAudioPort = i;
+        node1->connect_port( i, &audioBuffersIn[ AUDIO_BUFFER_AUDIO_INPUT ][0] );
       }
       else if( port.is_a( audio ) && port.is_a( output ) )
       {
-        audioBuffers[i] = std::vector< short >(audioBufferSize, 0);
-        node1->connect_port( i, &audioBuffers[i][0]);
-        idOutputAudioPort = i;
+        node1->connect_port( i, &audioBuffersIn[ AUDIO_BUFFER_AUDIO_OUTPUT ][0] );
       }
       
       else if( port.is_a( control ) && port.is_a( input ) )
       {
-        node1->connect_port( i, &controlInputBuffer[i] );
+        node1->connect_port( i, &audioBuffersIn[ AUDIO_BUFFER_CONTROL_INPUT ][0] );
       }
       else if( port.is_a( control ) && port.is_a( output ) )
       {
-        node1->connect_port( i, &controlOutputBuffer[i] );
+        node1->connect_port( i, &audioBuffersIn[ AUDIO_BUFFER_CONTROL_OUTPUT ][0] );
       }
+
       else
       {
         // unregonized port
@@ -111,25 +109,46 @@ public:
     int indexPlugin = std::distance( nodes.begin(), it );
     Lilv::Port port = plugins.at( indexPlugin ).get_port_by_symbol( pWorld->new_string(&portSymbol[0]) );
 
-    controlInputBuffer[ port.get_index() ] = value;
+    audioBuffersIn[ AUDIO_BUFFER_CONTROL_INPUT ][ port.get_index() ] = value;
   }
 
-  void processGraph( const short* bufferIn, short* bufferOut, int nbFrames )
+  void processGraph( const short* bufferIn, short* bufferOut )
   {
-    // int idInputAudioPort = 
-    // int idOutputAudioPort = 
+    // if the graph is empty
+    if ( nodes.size() == 0 )
+    {
+      memcpy( bufferOut, bufferIn, Lv2Graph::audioBufferSize );
+      return;
+    }
+
     // copy input
-    std::copy( bufferIn, bufferIn + nbFrames, &audioBuffers[idInputAudioPort][0] );
+    std::copy( bufferIn, bufferIn + Lv2Graph::audioBufferSize, &audioBuffersIn[ AUDIO_BUFFER_AUDIO_INPUT ][0] );
     // process nodes
     for ( unsigned int indexInstance = 0; indexInstance < nodes.size(); ++indexInstance )
     {
-      nodes.at(indexInstance)->run( nbFrames );
+      nodes.at(indexInstance)->run( Lv2Graph::audioBufferSize );
     }
     // copy output
-    memcpy( bufferOut, &audioBuffers[idOutputAudioPort][0], nbFrames );
+    memcpy( bufferOut, &audioBuffersIn[ AUDIO_BUFFER_AUDIO_OUTPUT ][0], Lv2Graph::audioBufferSize );
   }
 
+  /**
+  * Size of audio buffers.
+  */
+  static const unsigned int audioBufferSize = 128;
+
 private:
+  void initAudioBuffers()
+  {
+    audioBuffersIn = std::vector< std::vector< short > >( 4 );
+    audioBuffersOut = std::vector< std::vector< short > >( 4 );
+
+    audioBuffersIn[ AUDIO_BUFFER_AUDIO_INPUT ] = std::vector< short >( audioBufferSize, 0 );
+    audioBuffersIn[ AUDIO_BUFFER_AUDIO_OUTPUT ] = std::vector< short >( audioBufferSize, 0 );
+    audioBuffersIn[ AUDIO_BUFFER_CONTROL_INPUT ] = std::vector< short >( audioBufferSize, 0 );
+    audioBuffersIn[ AUDIO_BUFFER_CONTROL_OUTPUT ] = std::vector< short >( audioBufferSize, 0 );
+  }
+
   /**
   * Contains all data of all lv2 plugins on the system.
   */
@@ -147,16 +166,9 @@ private:
 
   /**
   * Audio buffers
-  * + TMP data
   */
-  std::vector< std::vector< short > > audioBuffers;
-  static const unsigned int audioBufferSize = 1024;
-  std::vector< short > controlInputBuffer;
-  std::vector< short > controlOutputBuffer;
-  
-  unsigned int idInputAudioPort;
-  unsigned int idOutputAudioPort;
-
+  std::vector< std::vector< short > > audioBuffersIn;
+  std::vector< std::vector< short > > audioBuffersOut;
 };
 
 /**
@@ -179,7 +191,6 @@ int main(int argc, char** argv)
   outfile = SndfileHandle( "testVFX.wav" , SFM_WRITE, format , numChannels , samplerate);
 
   Lv2Graph graph;
-
   // add nodes to the graph
   Lilv::Instance* gain = graph.addNode( "http://lv2plug.in/plugins/eg-amp", samplerate );
   // connect ports
@@ -195,10 +206,9 @@ int main(int argc, char** argv)
       break;
 
     // process graph
-    int nbFrames = 128;
     for (size_t i = 0; i < readedSamples; ++i) 
     {
-      graph.processGraph(&audioIn[ i ], &audioOut[ i ], nbFrames);
+      graph.processGraph( &audioIn[ i ], &audioOut[ i ] );
     }
 
     // write on disk
