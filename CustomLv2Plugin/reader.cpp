f279299e5ace28a0b6d335789834fb3745c6042a
Clement Champetier <cnt@mikrosimage.eu>
2014-05-16 19:12:15 +0200

    CustomLv2Plugin: add 2 ports to reader plugin
    * port 0 : atom port for the path of the input file (LV2_Atom_String)
    * port 2 : control port for the number of channels of the audio input
    * Update makefile to rebuild .so when update cpp
    * Tmp : the path of the input file is written in the plugin... Need to
    use the port 0 !

--- CustomLv2Plugin/reader.cpp
+++ CustomLv2Plugin/reader.cpp
@@ -1,35 +1,35 @@
+// SoX : manage IO
+#include "sox.h"
+
 // lv2-c++-tools : simplify Lv2 plugin creation
 #include <lv2plugin.hpp>
 
-// manage IO
-#include "sox.h"
+// Lv2 ext
+#include "lv2/lv2plug.in/ns/ext/atom/atom.h"
+#include "lv2/lv2plug.in/ns/ext/urid/urid.h"
 
 #include <string>
+#include <iostream>
 
+#define PLUGIN_READER_URI "http://ll-plugins.nongnu.org/lv2/lv2pftci/reader"
 
 using namespace LV2;
 
-
-class Reader : public Plugin<Reader> 
+class Reader : public Plugin< Reader > 
 {
 public:
 
-	Reader(double rate)
-		: Plugin<Reader>(1)
-		, _inputFile( "" )
-		, _outputStream( NULL )
+	Reader(double samplerate)
+		: Plugin< Reader >( 3 )
+		, _inputStream( NULL )
+		, _nChannels( 0 )
+		, _samplerate( samplerate )
 	{
 		// is safe here ?
 		sox_init();
-	}
-	
-	Reader(double rate, const std::string& inputFile)
-		: Plugin<Reader>(1)
-		, _inputFile( inputFile )
-		, _outputStream( NULL )
-	{
-		// is safe here ?
-		sox_init();
+		
+		// tmp
+		_inputFile = "/datas/cnt/workspace/ProtoAudio/CustomLv2Host/data/underwater.wav";
 	}
 
 	~Reader()
@@ -37,40 +37,76 @@ public:
 		sox_quit();
 	}
 
-	void setInputFile(const std::string& inputFile)
-	{
-		_inputFile = inputFile;
-	}
-
 	void activate()
 	{
-		if( _outputStream )
+		if( _inputStream )
 			return;
 		
-		_outputStream = sox_open_read(&_inputFile[0], NULL, NULL, NULL);
+		// get input file path from atom port 0
+		const LV2_Atom* atom = p< LV2_Atom >( 0 );
+		std::string sbody = (char*)LV2_ATOM_BODY( atom );
+		std::cout << "Plugin - inputPath : " << sbody << std::endl;
+		
+		// get number of channels form control port 2
+		_nChannels = *p(2);
+		std::cout << "Plugin - nbChannels : " << _nChannels << std::endl;
+		
+		// open the input file to set the input stream
+		_inputStream = sox_open_read( &_inputFile[0], NULL, NULL, NULL );
 	}
 	
-	void run(uint32_t nframes)
+	void run( uint32_t nframes )
 	{
-		sox_read(_outputStream, &_buffer[0], nframes);
-
-		// memcpy !!
-		for (uint32_t i = 0; i < nframes; ++i)
-			p(0)[i] = _buffer[i];
+		// support run with 0 frame
+		if( nframes == 0 )
+			return;
+		
+		// allocate buffer
+		if( _buffer.capacity() < nframes )
+			_buffer.resize( nframes );
+		
+		if( sox_read( _inputStream, &_buffer[0], nframes ) != SOX_EOF )
+		{
+			//memcpy( &p(1)[0], &_buffer[0], 1 );
+			for( uint32_t i = 0; i < nframes; ++i)
+			{
+				p(1)[i] = _buffer[i];
+			}
+		}
 	}
 	
 	void deactivate()
 	{
-		sox_close(_outputStream);
-		_outputStream = NULL;
+		sox_close( _inputStream );
+		_inputStream = NULL;
 	}
 	
 private :
+	/**
+	 * Path of the input file, from port 0 (Atom port).
+	 */
 	std::string 				_inputFile;
+	
+	/**
+	 * Number of channels of the input file, from port 2 (Control port).
+	 */
+	float						_nChannels;
+	
+	/**
+	 * Buffer when reading the input file.
+	 */
 	std::vector<sox_sample_t> 	_buffer;
 
-	sox_format_t* 				_outputStream;
+	/**
+	 * Input stream to read.
+	 */
+	sox_format_t* 				_inputStream;
+	
+	/**
+	 * Samplerate of the audio input.
+	 */
+	double						_samplerate;
 };
 
 
-static int _ = Reader::register_class("http://ll-plugins.nongnu.org/lv2/lv2pftci/reader");
+static int _ = Reader::register_class( PLUGIN_READER_URI );
